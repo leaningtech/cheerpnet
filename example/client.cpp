f76c51feac8a2ad7a092eb528c35aa46cdc36c62
Yuri Iozzelli <y.iozzelli@gmail.com>
2019-07-16 14:51:49 +0200

    Expand example with a round trip exchange

--- example/client.cpp
+++ example/client.cpp
@@ -15,7 +15,19 @@ extern "C" void run(client::String* key)
 	client::console.log("run()");
 	cheerpnet::Address srv = cheerpnet::resolve(key);
 	cheerpnet::SocketFD s = cheerpnet::socket();
-	int ret = cheerpnet::connect(s, srv, 555, [](cheerpnet::SocketFD s) {
+	int ret = cheerpnet::connect(s, srv, 555, cheerp::Callback([](cheerpnet::SocketFD s) {
 		client::console.log("connected!");
-	});
+		cheerpnet::recv_callback(s, cheerp::Callback([](cheerpnet::SocketFD s) {
+			uint8_t buf[256];
+			int len = cheerpnet::recv(s, buf, 256);
+			client::console.log("received back: ",(char*)buf);
+			cheerpnet::recv_callback(s, nullptr);
+			cheerpnet::close(s);
+			client::console.log("end");
+		}));
+		const char msg[] = "hello webrtc!";
+		int len = sizeof(msg);
+		client::console.log("sending: ", msg);
+		cheerpnet::send(s, (uint8_t*)msg, len);
+	}));
 }
--- example/server.cpp
+++ example/server.cpp
@@ -9,10 +9,21 @@ int main()
 	int ret = cheerpnet::listen(l, 555);
 	assert(ret==0 && "error listening");
 	client::console.log("key: ", cheerpnet::local_key());
-	cheerpnet::accept(l, [](cheerpnet::SocketFD fd, cheerpnet::Address a, cheerpnet::Port p)
+	cheerpnet::accept(l, cheerp::Callback([l](cheerpnet::SocketFD fd, cheerpnet::Address a, cheerpnet::Port p)
 	{
 		client::console.log("connected");
-	});
+		int ret = cheerpnet::recv_callback(fd, cheerp::Callback([l](cheerpnet::SocketFD fd) {
+			uint8_t buf[256];
+			int len = cheerpnet::recv(fd, buf, 256);
+			client::console.log("received : ",(char*)buf);
+			cheerpnet::recv_callback(fd, nullptr);
+			cheerpnet::send(fd, buf, len);
+			cheerpnet::close(fd);
+			cheerpnet::close(l);
+			client::console.log("end");
+		}));
+		assert(ret == 0);
+	}));
 	client::console.log("exit main");
 	return 0;
 }
